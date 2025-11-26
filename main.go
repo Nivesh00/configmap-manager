82542d0ffc9334a036fbfdda97d122f8dc143045
Nivesh <niveshramlochun055@gmail.com>
2025-11-26 23:53:13 +0100

    Extended kustomize folder

--- main.go
+++ main.go
@@ -22,10 +22,17 @@ func main() {
     http.HandleFunc("/validate-configmap-keys", module.HandleValidation)
     http.HandleFunc("/mutate-configmap-keys", module.HandleMutation)
 
-    slog.Info("listening on port :443")
-    err = http.ListenAndServeTLS(":443", "server.crt", "server.key", nil)
-    slog.Error("an error occured, stopping server", slog.Any("error", err))
-    os.Exit(1)
+    slog.Info("listening on port :3000")
+    if err = http.ListenAndServe(":3000", nil); err != nil {
+        slog.Error("a problem occured, server shutting down", slog.Any("error", err))
+        os.Exit(1)
+    }
+
+    // // TLS conf
+    // slog.Info("listening on port :443")
+    // err = http.ListenAndServeTLS(":443", "server.crt", "server.key", nil)
+    // slog.Error("an error occured, stopping server", slog.Any("error", err))
+    // os.Exit(1)
 }
 
 
